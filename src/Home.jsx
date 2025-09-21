b6f818662c84a23ebafed614da3c8eb5b952a5c0
anshuman morey <anshumanmorey2@gmail.com>
2025-09-21 20:08:43 +0530

    description modified

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
 return (
     <div>
         <h1 className="head">Playing with Emojis Using <span className='highlight-head'> React useState </span></h1>
-        <h2 className='sub-head'>this is a simple react app that allows you to play with emojis and express your feelings</h2>
+        <h2 className='sub-head'>this is a simple react app that allows you to play with emojis </h2>
         <div className='emoji-box' style={{ fontSize: `${slidervalue}px` }}>
             <div className='emoji'> {emoji}</div>
         </div>
