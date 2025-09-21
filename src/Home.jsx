240a6812fa542e8ec22e2ff25c93bde918c42314
anshuman morey <anshumanmorey2@gmail.com>
2025-09-21 18:25:33 +0530

    emoji box added

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,6 +4,9 @@ return (
     <div>
         <h1 className="head">Playing with Emojis Using <span className='highlight-head'> React useState </span></h1>
         <h2 className='sub-head'>this is a simple react app that allows you to play with emojis and express your feelings</h2>
+        <div className='emoji-box'>
+            <span className='emoji'>😀</span>
+        </div>
     </div>
 );
 }
