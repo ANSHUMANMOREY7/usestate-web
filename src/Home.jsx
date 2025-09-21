fd04d9fe685bbde4aecae2d38b55c834ef641e38
anshuman morey <anshumanmorey2@gmail.com>
2025-09-21 17:48:02 +0530

    Initial commit for Vite React project

---
+++ src/Home.jsx
@@ -0,0 +1,9 @@
+import './Home.css';
+function Home() {
+return (
+    <div>
+        <h1 className="head">Playing with Emojis Using <span className='highlight-head'> React useState </span></h1>
+    </div>
+);
+}
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import Home from './Home'
+import { createRoot } from 'react-dom/client'
+
+
+
+createRoot(document.getElementById('root')).render(
+
+    <Home />
+)
