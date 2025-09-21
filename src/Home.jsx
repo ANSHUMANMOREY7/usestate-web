2fd8ba9131a137c647e9f80d65bb233d1f511b19
anshuman morey <anshumanmorey2@gmail.com>
2025-09-21 18:33:20 +0530

    emojis addded

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,26 @@
+import { useState } from 'react';
 import './Home.css';
 function Home() {
+    const [emoji , setemoji]= useState("😀");
 return (
     <div>
         <h1 className="head">Playing with Emojis Using <span className='highlight-head'> React useState </span></h1>
         <h2 className='sub-head'>this is a simple react app that allows you to play with emojis and express your feelings</h2>
         <div className='emoji-box'>
-            <span className='emoji'>😀</span>
+            <div className='emoji'> {emoji}</div>
         </div>
+
+        <div className='emojis'> 😀 </div>
+        <div className='emojis'> 😁 </div>
+        <div className='emojis'> 😂 </div>
+        <div className='emojis'> 🤣 </div>
+        <div className='emojis'> 😃 </div>
+        <div className='emojis'> 😄 </div>
+        <div className='emojis'> 😆 </div>
+        <div className='emojis'> 😉 </div>
+        <div className='emojis'> 😊 </div>
+        <div className='emojis'> 😋 </div>
+
     </div>
 );
 }
