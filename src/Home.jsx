76d3ca0c260eda8a0fc5cc480f4bf6558f21dacc
anshuman morey <anshumanmorey2@gmail.com>
2025-09-22 15:03:48 +0530

    color picker added

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,31 +2,48 @@ import { useState } from 'react';
 import EmojiBtn from './EmojiBtn';
 import './Home.css';
 function Home() {
-    const [emoji , setemoji]= useState("😀");
-    const [slidervalue , setslidervalue] = useState("50")
-return (
-    <div className='body'>
-        <h1 className="head">Playing with Emojis Using <span className='highlight-head'> React useState </span></h1>
-        <h2 className='sub-head'>this is a simple react app that allows you to play with emojis </h2>
-        <div className='emoji-box' style={{ fontSize: `${slidervalue}px` }}>
-            <div className='emoji'> {emoji}</div>
-        </div>
-  <div className="emojis-container">
-        <EmojiBtn emoji={"😀"} setemoji={setemoji}/>
-        <EmojiBtn emoji={"😁"} setemoji={setemoji}/>
-        <EmojiBtn emoji={"😂"} setemoji={setemoji}/>
-        <EmojiBtn emoji={"🤣"} setemoji={setemoji}/>
-        <EmojiBtn emoji={"😃"} setemoji={setemoji}/>
-        <EmojiBtn emoji={"😄"} setemoji={setemoji}/>
-        <EmojiBtn emoji={"😆"} setemoji={setemoji}/>
-        <EmojiBtn emoji={"😉"} setemoji={setemoji}/>
-        <EmojiBtn emoji={"😊"} setemoji={setemoji}/>
-        <EmojiBtn emoji={"😋"} setemoji={setemoji}/>
-        </div>
- 
+    const [emoji, setemoji] = useState("😀");
+    const [slidervalue, setslidervalue] = useState("50")
+    return (
+        <div className='body'>
+            <h1 className="head">Playing with Emojis Using <span className='highlight-head'> React useState </span></h1>
+            <h2 className='sub-head'>this is a simple react app that allows you to play with emojis </h2>
+            <div className='emoji-box' style={{ fontSize: `${slidervalue}px` }}>
+                <div className='emoji'> {emoji}</div>
+            </div>
+            <div className="emojis-container">
+                <EmojiBtn emoji={"😀"} setemoji={setemoji} />
+                <EmojiBtn emoji={"😁"} setemoji={setemoji} />
+                <EmojiBtn emoji={"😂"} setemoji={setemoji} />
+                <EmojiBtn emoji={"🤣"} setemoji={setemoji} />
+                <EmojiBtn emoji={"😃"} setemoji={setemoji} />
+                <EmojiBtn emoji={"😄"} setemoji={setemoji} />
+                <EmojiBtn emoji={"😆"} setemoji={setemoji} />
+                <EmojiBtn emoji={"😉"} setemoji={setemoji} />
+                <EmojiBtn emoji={"😊"} setemoji={setemoji} />
+                <EmojiBtn emoji={"😋"} setemoji={setemoji} />
+            </div>
 
- <input type="range" className='slider' min="1" max="100" value={slidervalue} onChange={(e)=>{setslidervalue(e.target.value)}}/>
-   <span className='slider-value'>{slidervalue}</span>
-    </div>
-)}
+            <div className='color-picker'>
+                <div className='color'></div>
+                <div className='color'></div>
+                <div className='color'></div>
+                <div className='color'></div>
+                <div className='color'></div>
+                <div className='color'></div>
+                <div className='color'></div>
+                <div className='color'></div>
+                <div className='color'></div>
+                <div className='color'></div>
+
+
+            </div>
+
+            {/* <h2 className='sub-head slider-head'>Use the slider to change the size of the emoji</h2> */}
+
+            <input type="range" className='slider' min="1" max="100" value={slidervalue} onChange={(e) => { setslidervalue(e.target.value) }} />
+
+        </div>
+    )
+}
 export default Home;
